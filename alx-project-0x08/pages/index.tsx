538c8ff3be0b3c4c350d286adaa800a95c1a07ad
DevOsi123 <gatelogistics@consultant.com>
2025-06-22 07:00:52 -0700

    new

--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -14,7 +14,10 @@ const Home: React.FC = () => {
 
 
   const handleGenerateImage = async () => {
-    console.log("Generating Images");
+    const handleGenerateImage = async () => {
+  console.log("Generating Image");
+  console.log(process.env.NEXT_PUBLIC_GPT_API_KEY);
+};
   };
 
   return (
